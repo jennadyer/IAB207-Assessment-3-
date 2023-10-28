32b05ee394f953963d95ab9348149318acbb1894
jennadyer <114056523+jennadyer@users.noreply.github.com>
2023-10-29 02:07:20 +1000

    Add files via upload
    Added search bar functionality and filtering by category (on landing page)

--- a3_group98/MusicEventCentral/views.py
+++ a3_group98/MusicEventCentral/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Event
 from . import db
 
@@ -11,13 +11,14 @@ def index():
     return render_template('index.html', events=events)
 
 
-# @mainbp.route('/search')
-# def search():
-#     if request.args['search'] and request.args['search'] != "":
-#         print(request.args['search'])
-#         query = "%" + request.args['search'] + "%"
-#         destinations = db.session.scalars(db.select(Destination)).where(
-#             Destination.description.like(query))
-#         return render_template('index.html', destinations=destinations)
-#     else:
-#         return redirect(url_for('main.index'))
+# Search route. Search bar in header (in base.html).
+@bp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        events = db.session.scalars(
+            db.select(Event).where((Event.name.like(query)) | (Event.genre.like(query) | (Event.start_date.like(query)))))
+        return render_template('events/event_listing.html', events=events)
+    else:
+        return redirect(url_for('main.base'))
